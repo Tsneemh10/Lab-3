e940fb3419c663d4be40edd454f0b436eaddf22e
Tsneemh10 <68311792+Tsneemh10@users.noreply.github.com>
2022-05-21 21:34:40 +0200

    Add files via upload

---
+++ mainCoffee.java
@@ -0,0 +1,7 @@
+public class mainCoffee {
+    public static void main(String[] args) {
+        CoffeeMachine coff = new CoffeeMachine();
+        coff.insert_money(100);
+        coff.brew_coffee();
+    }
+}
